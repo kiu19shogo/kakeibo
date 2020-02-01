6b21f6a7486af794034dd1fa7e4f7cf9c399786c
kiu19shogo <kiu19shogo@gmail.com>
2020-02-01 15:06:53 +0900

    add "kakeibo" table meta

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -8,8 +9,14 @@ class Category(models.Model):
         db_table = "category"
 
     category_name = models.CharField(max_length=255, unique=True)
+
+
+class Kakeibo(models.Model):
+    class Meta:
+        db_table = "kakeibo"
     
+    date = models.DataField(verbose_name="日付", default=datetime.now)
+    category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
+    money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
+    memo = models.CharField(verbose_name="メモ", max_length=500)
 
-
-
-    
